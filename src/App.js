3d21362c4bd75b0e617d8801c6f45fd967410f5d
Chris Steib <christopher.g.steib@gmail.com>
2020-08-04 06:55:07 -0700

    pushed back to original App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          I was changed on the Feature branch
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+	  Hi THere!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Changed in feature
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
